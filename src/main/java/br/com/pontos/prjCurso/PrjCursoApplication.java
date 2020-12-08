101436e6b02dc6b1090afd04c422d78ce5511409
Andre Gurjao <agurjao@hotmail.com>
2020-12-08 21:59:21 +0000

    Prj criado

---
+++ src/main/java/br/com/pontos/prjCurso/PrjCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pontos.prjCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pontos/prjCurso/PrjCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pontos.prjCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
