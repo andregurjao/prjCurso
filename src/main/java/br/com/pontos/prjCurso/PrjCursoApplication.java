c2f128b719053580cdbdd2c03bc6b4f80013b643
Andre Gurjao <agurjao@hotmail.com>
2020-12-11 16:59:11 +0000

    Criacao oper de instanciacao

--- src/main/java/br/com/pontos/prjCurso/PrjCursoApplication.java
+++ src/main/java/br/com/pontos/prjCurso/PrjCursoApplication.java
@@ -1,13 +1,30 @@
 package br.com.pontos.prjCurso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PrjCursoApplication {
+import br.com.pontos.prjCurso.domain.Categorias;
+import br.com.pontos.prjCurso.repositories.CategoriasRepository;
 
+@SpringBootApplication
+public class PrjCursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriasRepository catRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrjCursoApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		Categorias cat1 = new Categorias(null, "Informatica");
+		Categorias cat2 = new Categorias(null, "Escritorio");
+
+		catRepo.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
